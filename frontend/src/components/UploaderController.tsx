f74c9710ee25901a0adfa2a1b218542558675892
Julio Aguilar <julio.aguilar9928@gmail.com>
2023-07-14 13:42:04 -0600

    add select files from button

--- frontend/src/components/UploaderController.tsx
+++ frontend/src/components/UploaderController.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import DragAndDropArea from './DragAndDropArea'
 
 function UploaderController() {
+  const inputFile = useRef<HTMLInputElement>(null)
+
   const handleSelectedFile = (file: File) => {
     console.log(file)
+  }
+
+  const handleChooseFileClick = () => {
+    inputFile.current?.click()
   }
 
   return (
@@ -14,6 +20,22 @@ function UploaderController() {
       </header>
 
       <DragAndDropArea onSelectedFile={handleSelectedFile} />
+
+      <footer className="mt-4 flex flex-col items-center gap-y-4">
+        <p className="text-sm text-[#828282]">Or</p>
+        <input
+          ref={inputFile}
+          type="file"
+          hidden
+          onChange={(e) => handleSelectedFile(e.target.files![0])}
+        />
+        <button
+          className="bg-[#2F80ED] hover:opacity-90 min-w-max min-h-max px-4 py-2 rounded-lg text-white text-md whitespace-nowrap"
+          onClick={handleChooseFileClick}
+        >
+          Choose a file
+        </button>
+      </footer>
     </section>
   )
 }
