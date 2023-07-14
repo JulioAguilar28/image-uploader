ab7b671b34f86b1c46a77a09596a8ed6bd6af73d
Julio Aguilar <julio.aguilar9928@gmail.com>
2023-07-13 20:14:52 -0600

    serve fronted app as static page

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,11 +1,16 @@
 require('dotenv').config()
 const express = require('express')
+const path = require('path')
 
 const AuthRouter = require('./auth/auth_router')
 const ImagesRouter = require('./images/images_router')
 
 const app = express()
 const port = 3000
+
+// frontend app and static files
+const staticPath = path.join(__dirname, '../dist')
+app.use(express.static(staticPath))
 
 // Middlewares
 app.use(express.json())
@@ -14,6 +19,10 @@ app.use(express.urlencoded({ extended: true }))
 // Api routes
 app.use('/api/v1', AuthRouter)
 app.use('/api/v1/images', ImagesRouter)
+
+app.use('/*', (req, res) => {
+  res.sendFile(path.join(staticPath, 'index.html'))
+})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
