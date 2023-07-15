7c41ecdc43e84515c97551a9fbac09c2651a9627
Julio Aguilar <julio.aguilar9928@gmail.com>
2023-07-15 09:42:23 -0600

    add header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css'
+import Header from './components/Header'
 import UploaderController from './components/UploaderController'
 import AuthController from './components/auth/AuthController'
 
 function App() {
   return (
-    <main className="h-screen w-full flex justify-center items-center">
-      <section className="max-h-[520px] w-[400px] px-8 py-9 rounded-xl shadow-xl shadow-slate-300">
-        <AuthController />
-        {/* <UploaderController /> */}
+    <main className="h-screen w-full flex flex-col items-center">
+      <Header />
+
+      <section className="grow w-full flex flex-col items-center justify-center">
+        <div className="max-h-[520px] w-[400px] px-8 py-9 rounded-xl shadow-xl shadow-slate-300">
+          {/* <AuthController /> */}
+          <UploaderController />
+        </div>
       </section>
     </main>
   )
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="w-full flex items-center justify-between h-16 px-4 shadow-md shadow-slate-300">
+      <span className="text-xl text-[#4F4F4F]">Image Uploader</span>
+      <span className="text-base text-[#4F4F4F]">Welcome Julio Aguilar</span>
+    </header>
+  )
+}
+
+export default Header
